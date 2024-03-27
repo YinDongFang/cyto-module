9ff71b81cb7d14c5a58fc1645202081f60a2f176
Yin <doyin@deloitte.com.cn>
2024-03-27 17:45:42 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,8 @@
+const deepCopy = require('./deepCopy')
+const isPalindrome = require('./isPalindrome')
+const stringToNumberArray = require('./stringToNumberArray')
+const getMinValue = require('./getMinValue')
+
+module.exports = (str) => {
+    return str.toUpperCase();
+  }
